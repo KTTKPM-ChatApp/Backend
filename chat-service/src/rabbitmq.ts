8c495f7d19e7014abdb521d99db7583a99c05731
nguyen-an-nguyen-dev <Nguyenannguyen8889@gmail.com>
2026-05-31 22:00:33 +0700

    fix(backend): bind RabbitMQ consumer queue to reaction added and removed events

--- chat-service/src/rabbitmq.ts
+++ chat-service/src/rabbitmq.ts
@@ -137,6 +137,8 @@ export async function startConsumer(
 
     await channel.bindQueue(queue.queue, EXCHANGE_NAME, RABBITMQ.ROUTING_KEYS.MESSAGE_SENT);
     await channel.bindQueue(queue.queue, EXCHANGE_NAME, RABBITMQ.ROUTING_KEYS.MESSAGE_DELETED);
+    await channel.bindQueue(queue.queue, EXCHANGE_NAME, RABBITMQ.ROUTING_KEYS.REACTION_ADDED);
+    await channel.bindQueue(queue.queue, EXCHANGE_NAME, RABBITMQ.ROUTING_KEYS.REACTION_REMOVED);
     await channel.bindQueue(queue.queue, EXCHANGE_NAME, 'user.*');
 
     await channel.consume(queue.queue, async (msg) => {
